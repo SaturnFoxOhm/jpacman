7a2454e7e045e74fd7a683c59cbbeca291b50cab
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2023-11-02 13:35:12 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -5,6 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
+/**
+* Returns an Image object that can then be painted on the screen. 
+* The url argument must specify an absolute <a href="#{@link}">{@link URL}</a>. The name
+* argument is a specifier that is relative to the url argument. 
+* <p>
+* This method always returns immediately, whether or not the 
+* image exists. When this applet attempts to draw the image on
+* the screen, the data will be loaded. The graphics primitives 
+* that draw the image will incrementally paint on the screen. 
+*
+* @param  url  an absolute URL giving the base location of the image
+* @param  name the location of the image, relative to the url argument
+* @return      the image at the specified URL
+* @see         Image
+*/
+
 class BoardTest {
     private static final int MAX_WIDTH = 1;
     private static final int MAX_HEIGHT = 1;
